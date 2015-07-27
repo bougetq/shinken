a0fb5ba32f8fa6f124e106850f28836c4be43988
Quentin Bouget <quentin.bouget.ocre@cea.fr>
2015-07-27 22:20:29 +0200

    Enh: full NodeSetParse

--- shinken/objects/nodesetparse.py
+++ shinken/objects/nodesetparse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding : utf-8 -*-
+# -*- coding: utf-8 -*-
 
 # author:
 #   Quentin Bouget, quentin.bouget.ocre@cea.fr
@@ -9,6 +9,10 @@
 """
 Module to parse shinken configuration files according to
 ClusterShell.NodeSet syntax
+
+In this documentation a macro-function designate a macro that takes
+arguments. Its syntax is:
+    \\$[^($]*\\(ARGS\\)\\$
 """
 
 from ClusterShell.NodeSet import NodeSet
@@ -22,11 +26,12 @@ def process(obj):
     """
     Main function of the module, returns a list of processed objects
 
+    Duplicates objects where there are duplicate macro-functions
     Expands every property according to ClusterShell.NodeSet syntax
     """
 
-    obj_list = duplicate(obj)
-    for obj in obj_list:
+    obj_lst = duplicate(obj)
+    for obj in obj_lst:
         for prop in obj:
             for i in range(0, len(obj[prop])):
                 obj[prop][i] = expand(obj[prop][i])
@@ -35,172 +40,100 @@ def process(obj):
                 if is_empty(obj[prop][i]):
                     del obj[prop][i]
 
-    return obj_list
-
-def expand(value):
-    """Wrapper function for repl_macro_function with expand_repl_func"""
-    return repl_macro_function(value,
-                               EXPAND_MACRO_NAME,
-                               True,
-                               expand_repl_func,
-                               ',')
+    # Main debug
+    #print obj_lst
+    return obj_lst
 
 def is_empty(value):
     """Check wether or not value is only whitespaces (empty)"""
     return re.match(r'^\s*$', value)
 
-def check_ends_with_dollar(macro):
+def find_macro_func(macro_name, value):
+    """Parser that finds the first occurence of the macro-function
+    named macro_name and returns some informations about it
     """
-    Raise an error if the macro matched by
-    find_macro ends with a dollar
-    """
-    if not macro['end_dollar']:
-        raise SyntaxError("Macro %s does not end with a dollar" \
-                          % macro['value'])
-
-def find_macro(value):
-    """
-    Parser that finds the first macro-like expression in value and
-    returns useful information about it.
-
-    Matches:
-        \\$.*?\\$
-        \\$[^(]*\\(ARGS\\)\\$
-            where ARGS is a string that contains an arbitrary number of
-            nested parentheses
-        \\$.*?\\s
-    """
-
-    opened_parens = 0
-    args = None
-    start_macro = value.find('$')
-    end_macro = len(value)-1
-    # We found a macro
-    if start_macro >= 0:
-        end_dollar = False
-        for i in range(start_macro+1, len(value)):
-            if value[i] == '(':
-                opened_parens += 1
-                if not args:
-                    # The first parenthese
-                    args = {'parens_open': i, 'parens_close': None}
-                elif args['parens_close']:
-                    # There can only be one set of parenthese at the first
-                    # level
-                    raise SyntaxError("Invalid syntax, only one block of " +\
-                            "parentheses is allowed inside a given macro " +\
-                            "at: %s" % value)
-            elif value[i] == ')':
-                if opened_parens > 0:
+    val = value
+    start = 0
+    while val.find(macro_name) > 0:
+        mobj = re.search(macro_name, val)
+        if val[mobj.start(0) - 1] == '$' and val[mobj.end(0)] == '(':
+            opened_parens = 1
+            args = ''
+            for i in range(mobj.end(0)+1, len(val)):
+                char = val[i]
+                if char == '(':
+                    opened_parens += 1
+                elif char == ')':
                     opened_parens -= 1
-                    if opened_parens == 0 and not args['parens_close']:
-                        # The first level set of parentheses is closed
-                        args['parens_close'] = i
-                        # What was inside it
-                        args['value'] = value[args['parens_open']+1:i]
-                else:
-                    # This is a closing parenthese before an opening one
-                    raise SyntaxError("Too early closing parenthese at: %s" %\
-                            value)
-            elif re.match(r'\s', value[i]) and opened_parens == 0:
-                # We count '\s' as a terminating character because
-                # we consider expressions like '\$.*?\s?$' also need to be
-                # protected before we use NodeSet to expand values
-                end_macro = i
-                break
-            elif value[i] == '$' and opened_parens == 0:
-                end_dollar = True
-                end_macro = i
-                break
-        # We read everything in value but could not close
-        # all the set of parentheses
-        if args and not args['parens_close']:
-            raise SyntaxError("Unclosed parentheses in macro at: %s" % value)
+                    if opened_parens == 0:
+                        break
+                args += char
+            end = i + 1
+            if opened_parens == 0 and val[end] == '$':
+                return {'start': start + mobj.start(0) - 1,
+                        'end': start + end,
+                        'args': args}
 
-        # Everything went well
-        macro_value = value[start_macro:end_macro+1]
-        return {'start_macro': start_macro,
-                'end_macro': end_macro,
-                'name': re.search(r'\$([^($\s]*)', macro_value).group(1),
-                'args': args,
-                'value': macro_value,
-                'end_dollar': end_dollar}
+        start += mobj.end(0)+1
+        val = value[start:]
     return None
 
-def repl_macro_function(value, macro_name, ends_with_dollar, rep_func,
-                        *args, **kwargs):
+def expand(value):
     """
-    Utility function to replace a macro-function with the result of
-    the rep_func when given the arguments of the macro-function
-
-    By macro-function we designate the second kind of macro matched
-    find_macro function above.
-    The macro-function must take exactly one argument which must be
-    a string.
+    Expands every macro-function named EXPAND_MACRO_NAME by the
+    expanded version of its arguments
     """
 
     start = 0
     val = ''
-    while start < len(value):
-        macro = find_macro(value[start:])
-        if not macro:
-            # No (more) macro in value
-            val += value[start:]
-            break
-        elif not macro['args'] or not re.match(macro_name, macro['name']):
-            # Not interested in this macro
-            val += value[start:start + macro['end_macro']+1]
-            start += macro['end_macro']+1
-        else:
-            # Found a macro like we were looking for
-            if ends_with_dollar:
-                check_ends_with_dollar(macro)
+    macro = find_macro_func(EXPAND_MACRO_NAME, value)
+    while macro:
+        # Found a macro like the one we are looking for
 
-            val += value[start:macro['start_macro']]
+        # Append what is before it in value
+        val += value[start:start + macro['start']]
 
-            # Apply the rep_func on the args of the macro-function
-            if len(args) and len(kwargs):
-                val += rep_func(macro['args']['value'], args, kwargs)
-            elif len(args):
-                val += rep_func(macro['args']['value'], args)
-            elif len(kwargs):
-                val += rep_func(macro['args']['value'], kwargs)
-            else:
-                val += rep_func(macro['args']['value'])
+        # Append the processed args
+        val += expand_nodeset(macro['args'])
 
-            start += macro['end_macro']+1
+        # Move forward in value
+        start += macro['end']+1
+        macro = find_macro_func(EXPAND_MACRO_NAME, value[start:])
+
+    # Append what is left of value
+    val += value[start:]
 
     return val
 
-def split_preserve_range(value, sep=","):
+def split_preserve(value, sep=","):
     """
-    Split value on sep if sep is not inside brackets.
+    Split value on sep if sep is not inside brackets or parentheses
 
     Sep must be a single char
     """
     val_lst = ['']
     opened_bracket = 0
+    opened_parens = 0
     for char in value:
         if char == '[':
             opened_bracket += 1
         elif char == ']':
             opened_bracket -= 1
+        if char == '(':
+            opened_parens += 1
+        if char == ')':
+            opened_parens -= 1
         elif char == sep:
-            if not opened_bracket:
+            if not opened_bracket and not opened_parens:
                 val_lst += ['']
                 continue
         val_lst[-1] += char
 
     return val_lst
 
-def expand_repl_func(value, args):
+def expand_nodeset(value, sep=','):
     """Expands a value to a string according to ClusterShell.NodeSet
     syntax and joins the result with the separator sep"""
-
-    if len(args):
-        sep = args[0]
-    else:
-        sep = ','
 
     if isinstance(value, list):
         return [expand(elt) for elt in value]
@@ -213,7 +146,7 @@ def expand_repl_func(value, args):
     # Shinken does not mind if you give it the same value several times.
     # That way we can have our own union operator, that does not remove
     # duplicated values but preserve the order
-    value_tmp = split_preserve_range(value)
+    value_tmp = split_preserve(value)
     val_tmp = []
     for val in value_tmp:
         val_tmp.extend([node for node in NodeSet(val)])
@@ -236,20 +169,6 @@ def copy_obj(obj):
         new_obj[prop] = obj[prop][:]
     return new_obj
 
-def _append_all(clone_lst, prop, idx, value, reinit=False):
-    """
-    Utility function to append a string to all the element of a
-    clone list as it is defined in duplicate
-
-    Created to reduce the number of branches in duplicate
-    """
-    if reinit:
-        for clone in clone_lst:
-            clone[prop][idx] = ''
-    else:
-        for clone in clone_lst:
-            clone[prop][idx] += value
-
 def duplicate(obj):
     """
     Duplicate a shinken object on the macro matching the
@@ -261,63 +180,59 @@ def duplicate(obj):
         for i in range(0, len(obj[prop])):
             start = 0
             value = obj[prop][i]
-            macro = find_macro(value)
+            macro = find_macro_func(DUPLICATE_MACRO_NAME, value)
             reinit_once = False
 
-            while macro and value.find(DUPLICATE_MACRO_NAME) > 0:
+            while macro:
                 if not reinit_once:
                     # re-init the prop
                     obj_template[prop][i] = ''
-                    _append_all(clone_lst, prop, i, None, reinit=True)
+                    for clone in clone_lst:
+                        clone[prop][i] = ''
 
                     reinit_once = True
 
-                if macro['args'] \
-                    and re.match(DUPLICATE_MACRO_NAME, macro['name']):
+                # pfx = what is before the macro
+                pfx = value[start:start+macro['start']]
 
-                    check_ends_with_dollar(macro)
+                # Expands what needs to be expanded
+                values = expand(macro['args'])
 
-                    # pfx = what is before the macro
-                    pfx = value[start:start+macro['start_macro']]
+                # Find the different duplicate values
+                values = split_preserve(values)
+                # Remove useless whitespaces
+                values = [val.strip() for val in values]
 
-                    # Expands what needs to be expanded
-                    macro['args']['value'] = expand(macro['args']['value'])
+                for j in range(0, len(values)):
+                    # Add a clone if needed
+                    if j == len(clone_lst):
+                        clone_lst.append(copy_obj(obj_template))
 
-                    # Find the different duplicate values
-                    values = macro['args']['value'].split(',')
-                    # Remove useless whitespaces
-                    values = [val.strip() for val in values]
+                    # Append the new_value
+                    clone_lst[j][prop][i] += pfx + values[j]
 
-                    for j in range(0, len(values)):
-                        # Add a clone if needed
-                        if j == len(clone_lst):
-                            clone_lst.append(copy_obj(obj_template))
+                last_value = pfx + values[-1]
 
-                        # Append the new_value
-                        clone_lst[j][prop][i] += pfx + values[j]
+                # More clone than values
+                for j in range(len(values), len(clone_lst)):
+                    clone_lst[j][prop][i] += last_value
 
-                    last_value = pfx + values[-1]
-
-                    # More clone than values
-                    for j in range(len(values), len(clone_lst)):
-                        clone_lst[j][prop][i] += last_value
-
-                    # To deal with multiple duplicate macro
-                    # on the same line of the same property
-                    obj_template[prop][i] += last_value
-                else:
-                    # Not the macro we are looking for => append it as it is
-                    _append_all(clone_lst, prop, i,
-                                value[start:start + macro['end_macro'] + 1])
+                # To deal with multiple duplicate macro
+                # on the same line of the same property
+                obj_template[prop][i] += last_value
 
                 # Move forward in value
-                start += macro['end_macro'] + 1
-                macro = find_macro(value[start:])
+                start += macro['end'] + 1
+                macro = find_macro_func(DUPLICATE_MACRO_NAME, value[start:])
 
             # No more macros
             if reinit_once:
                 # We re-inited the prop => append what is left of the value
-                _append_all(clone_lst, prop, i, value[start:])
+                for clone in clone_lst:
+                    clone[prop][i] += value[start:]
+
+                # Need to append it to the template too for future clones
+                obj_template[prop][i] += value[start:]
 
     if clone_lst == []:
         # There was no duplicate macro => return [obj]
--- test/test_nodesetparse.py
+++ test/test_nodesetparse.py
@@ -27,32 +27,91 @@ class TestNodesetparse(ShinkenTest):
         self.assertTrue(self.conf.conf_is_correct,
                         "The configuration was deemed incorect")
 
+    def hosts_from_name_list(self, name_lst):
+        return [self.sched.hosts.find_by_name(name) for name in name_lst]
+
+    def hostgroups_from_name_list(self, name_lst):
+        return [self.sched.hostgroups.find_by_name(name) for name in name_lst]
+
     def test_simple_expand(self):
-        expected_members = ['h_0_0', 'h_0_1', 'h_0_2', 'h_0_3', 'h_0_4']
-        expected_members = [self.sched.hosts.find_by_name(name)\
-                            for name in expected_members]
+        name_lst = ['h_0_0', 'h_0_1', 'h_0_2']
+        expected_members = self.hosts_from_name_list(name_lst)
         hg_0_0 = self.sched.hostgroups.find_by_name('hg_0_0')
         self.assertIsNotNone(hg_0_0)
         for host in hg_0_0.members:
-            self.assertTrue(host in expected_members,
-                            'unexpected member in hg_0_0')
+            self.assertIn(host, expected_members)
             expected_members.remove(host)
-        self.assertEqual([], expected_members, 'missing members in hg_0_0')
+        self.assertEqual([], expected_members)
+
+    def test_expand_with_commas(self):
+        name_lst = ['h_0_0', 'h_0_1', 'h_0_2']
+        expected_members = self.hosts_from_name_list(name_lst)
+        hg_1_0 = self.sched.hostgroups.find_by_name('hg_1_0')
+        self.assertIsNotNone(hg_1_0)
+        for host in hg_1_0.members:
+            self.assertIn(host, expected_members)
+            expected_members.remove(host)
+        self.assertEqual([], expected_members)
 
     def test_nodeset_op(self):
-        hostgroup = self.sched.hostgroups.find_by_name('hg_8_0')
-        self.assertIsNotNone(hostgroup, 'missing hostgroup hg_8_0')
-        expected_members = [self.sched.hosts.find_by_name('h_8_' + str(i))\
-                            for i in range(60, 70)]
-        for member in hostgroup.members:
-            self.assertTrue(member in expected_members,\
-                            'unexpected member %s for hostgroup hg_8_0' %\
-                            member.host_name)
-            expected_members.remove(member)
+        hostgroup = self.sched.hostgroups.find_by_name('hg_2_0')
+        self.assertIsNotNone(hostgroup)
+        name_lst = ['h_2_' + str(i) for i in range(60, 70)]
+        expected_members = self.hosts_from_name_list(name_lst)
+        for host in hostgroup.members:
+            self.assertIn(host, expected_members)
+            expected_members.remove(host)
+        self.assertEqual([], expected_members)
 
-        self.assertEqual([], expected_members,\
-                         'missing member(s) for hostgroup hg_8_0')
+    def test_simple_duplicate(self):
+        name_lst = ['h_3_' + str(i) for i in range(0, 3)]
+        expected_hosts = self.hosts_from_name_list(name_lst)
+        for host in expected_hosts:
+            self.assertIsNotNone(host)
 
+    def test_duplicate_matching_props(self):
+        name_lst = ['h_4_' + str(i) for i in range(0, 3)]
+        expected_hosts = self.hosts_from_name_list(name_lst)
+        for i in range(0, 3):
+            host = expected_hosts[i]
+            self.assertIsNotNone(host)
+            self.assertEqual(host.address, '127.0.4.' + str(i+1))
+
+    def test_nested_expand_in_duplicate(self):
+        name_lst = ['h_5_' + str(i) for i in range(0, 9)]
+        expected_hosts = self.hosts_from_name_list(name_lst)
+        for i in range(0, 9):
+            host = expected_hosts[i]
+            self.assertIsNotNone(host)
+            self.assertEqual(host.address, '127.0.5.' + str(i+1))
+
+    def test_nested_duplicate_in_expand(self):
+        name_lst = ['h_6_0', 'h_6_1']
+        hosts = self.hosts_from_name_list(name_lst)
+        name_lst = ['hg_6_0_' + str(i) for i in range(0, 10)]
+        hgs_0 = self.hostgroups_from_name_list(name_lst)
+        name_lst = ['hg_6_1_' + str(i) for i in range(0, 10)]
+        hgs_1 = self.hostgroups_from_name_list(name_lst)
+        for hg in hgs_0:
+            self.assertIsNotNone(hg)
+            self.assertIn(hosts[0], hg.members)
+        for hg in hgs_1:
+            self.assertIsNotNone(hg)
+            self.assertIn(hosts[1], hg.members)
+
+    def test_expand_multi_line(self):
+        name_lst = ['h_8_0', 'h_8_1', 'h_8_2']
+        expected_members = self.hosts_from_name_list(name_lst)
+        hg_8_0 = self.hostgroups_from_name_list(['hg_8_0'])[0]
+        self.assertIsNotNone(hg_8_0)
+        for host in hg_8_0.members:
+            self.assertIn(host, expected_members)
+            expected_members.remove(host)
+        self.assertEqual([], expected_members)
+
+    def test_empty_duplicate(self):
+        h_9_0 = self.hosts_from_name_list(['h_9_0'])[0]
+        self.assertEqual([], h_9_0.hostgroups)
 
 if __name__ == '__main__':
     unittest.main()
